de9a15f837782266aa4ccc563d64184e1f09fcee
DevBadi <ayoubbouchedoub@gmail.com>
2022-02-08 21:40:21 +0100

    Add SignUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  error: string = '';
+  constructor(private authService: AuthService){}
   title = 'angular-auth';
+  onSubmit(form: NgForm){
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authService.signIn(email, password).subscribe(resData => {
+      console.log(resData);
+    }, error =>{
+      this.error = ' we have an issue in the backend';
+      console.log(error);
+    });
+    //console.log(form.value);
+    form.reset;
+  }
 }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+interface AuthResponseData{
+    AccessJWT: string,
+    RefreshJWT: string,
+    FirstName: string,
+    LastName: string,
+    Position: string,
+    SiteLan: string
+
+}
+@Injectable({providedIn: "root"})
+export class AuthService{
+    constructor(private http: HttpClient){}
+    
+    signIn(email: string, password: string){
+        return this.http.post<AuthResponseData>('https://la.pilinux.me/api/v0.1.0/register',
+        {
+            email: email,
+            password: password
+        }
+        );
+    }
+}
